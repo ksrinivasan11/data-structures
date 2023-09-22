359bdb23a32a11c215c0427cfcdeccc543aff6e5
ksrinivasan11 <ksrinivasan@stu.naperville203.org>
2023-09-22 13:25:56 -0500

    worked on and finished sieve

--- Chapter 15 Activities/Sieve/src/Sieve.java
+++ Chapter 15 Activities/Sieve/src/Sieve.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
-
+import java.util.*;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 /**
  * A program that implements the sieve of Eratosthenes.
@@ -11,10 +16,31 @@ public class Sieve
         Scanner in = new Scanner(System.in);
         System.out.println("Compute primes up to which integer?");
         int n = in.nextInt();
+        
+        Set<Integer> set = new HashSet<>();
+        Set<Integer> primes = new HashSet<>();
 
-        // Your work goes here
-        . . .
 
+        for (int i = 2; i <= n; i++){
+            set.add(i);
+        }
+        int count = 0;
+        for (Integer setNum1: set) {
+            for (Integer setNum2: set){
+                
+                if (setNum1 % setNum2 != 0){
+                    count++;
+                    System.out.println(setNum1 + "/" + setNum2 + ": " + count);
+                }
+            }
+            if (count == set.size()-1){
+                primes.add(setNum1);
+                
+            }
+            count = 0;
+        }
+        System.out.println(primes);
+    
 
 
 

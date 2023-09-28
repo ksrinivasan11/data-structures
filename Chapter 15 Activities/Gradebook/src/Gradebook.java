2246cfab6e6ea088e8488028154b2acc4f34fb56
ksrinivasan11 <111408308+ksrinivasan11@users.noreply.github.com>
2023-09-28 13:30:35 -0500

    g

--- Chapter 15 Activities/Gradebook/src/Gradebook.java
+++ Chapter 15 Activities/Gradebook/src/Gradebook.java
@@ -1,5 +1,8 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
-. . .
+import java.util.Set;
+
 /**
  * A program to add, remove, modify or print
  * student names and grades.
@@ -10,7 +13,7 @@ public class Gradebook
     {
         Scanner in = new Scanner(System.in);
 
-        . . .
+        Map<String, String> grades = new HashMap<> ();
 
         boolean done = false;
         while(!done)
@@ -22,17 +25,39 @@ public class Gradebook
                 done = true;
             } else if (input.equals("A"))
             {
-                . . .
+                System.out.println("What is the name of the student?");
+                String name = in.next().toUpperCase();
+                
+                System.out.println("What is their grade?");
+                String grade = in.next().toUpperCase();
+
+                grades.put(name, grade);
 
             } else if (input.equals("R"))
             {
-                . . .
+                System.out.println("Who do you want to remove?");
+                String student = in.next().toUpperCase();
+
+                grades.remove(student);
+
+
             } else if (input.equals("M"))
             {
-                . . .
+                System.out.println("What is the name of the student?");
+                String name = in.next().toUpperCase();
+                
+                System.out.println("What is their new grade?");
+                String grade = in.next().toUpperCase();
+
+                grades.put(name, grade);
+
             } else if (input.equalsIgnoreCase("P"))
             {
-                . . .
+                Set<String> keys = grades.keySet();
+
+                for (String key: keys){
+                System.out.println("Name: " + key + " Grade: " + grades.get(key)); }
+
             } else
             {
                 done = true;

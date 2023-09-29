4d9ad5ff8571d41ad0b51d55990ccaa9a25bc71c
ksrinivasan11 <111408308+ksrinivasan11@users.noreply.github.com>
2023-09-29 13:15:33 -0500

    str

--- Chapter 15 Activities/StringLengthMap/src/StringLengthMap.java
+++ Chapter 15 Activities/StringLengthMap/src/StringLengthMap.java
@@ -12,13 +12,13 @@ public class StringLengthMap
 {
     public static void main(String[] args) throws FileNotFoundException
     {
-        String filename = "src/test1.txt";
+        String filename = "Chapter 15 Activities/StringLengthMap/src/test1.txt";
 
         try (Scanner in = new Scanner(new File(filename)))
         {
 
             // Create your map here
-            
+            Map<Integer, String> striLen = new HashMap<> ();
 
             while (in.hasNext())
             {
@@ -28,12 +28,14 @@ public class StringLengthMap
                 // Update the map here
                 // Modify Worked Example 15.1
                 
-
+                striLen.put(len, word);
 
             }
 
             // Print the strings, in increasing order of their length
             // Use this format: 1: i, a, i
+
+            System.out.println(striLen);
         } catch (FileNotFoundException e)
         {
             System.out.println("Cannot open: " + filename);

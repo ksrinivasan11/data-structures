77b7c9de5687c79055e12cf67b40a42d64e66f0c
ksrinivasan11 <111408308+ksrinivasan11@users.noreply.github.com>
2023-10-03 13:31:05 -0500

    sqpq

--- Chapter 15 Class Notes/src/QueueDemo.java
+++ Chapter 15 Class Notes/src/QueueDemo.java
@@ -9,5 +9,30 @@ public class QueueDemo
 {
     public static void main(String[] args)
     {
+        // Create a print queue of strings (using a linked list)
+        Queue<String> jobs = new LinkedList<>();
+
+        // Add some print jobs
+        jobs.add("Joe: Expense Report 2023");
+        jobs.add("Cathy: Top Secrete Document #5");
+
+        System.out.println("Printing: " + jobs.remove());
+
+        jobs.add("Cathy: Really Top Secrete Document #2");
+        jobs.add("Joe: Grocery List");
+        jobs.add("Cathy: Can I Get Fired For This?");
+
+        System.out.println("Printing: " + jobs.remove());
+
+        jobs.add("Boss : Cathy's Termination Letter");
+
+        int k = jobs.size();
+        for (int i = 0; i<k; i++){
+            System.out.println("Printing: " + jobs.remove());
+        }
+
+        // Print the rest of the jobs in the queue
+
+
     }
 }
